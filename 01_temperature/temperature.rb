e83fc7dfffc3834f0ed7869ac82077b555e54b7a
Len Lee <lenlee454@gmail.com>
2016-09-22 14:09:58 -0700

    answer

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc(f)
+    (f-32)*5/9
+end
+
+def ctof(c)
+    c*1.8 + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
 #write your code here
+def add(x,y)
+    x + y
+end
+
+def subtract(x,y)
+    x-y
+end
+
+def sum(array)
+    total =0
+    array.each do |i| 
+        total = total+i
+    end
+    return total
+end
+
+def multiply(array)
+    total = 1
+    array.each do |i|
+        total *= i
+    end
+    total
+end
+
+def power(a,b)
+    a**b
+end
+
+def factorial(a)
+    result = 1
+    (1..a).to_a.each do |x|
+        result *=x
+    end
+    return result
+end
