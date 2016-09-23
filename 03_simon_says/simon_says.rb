9f0f9dd7a8d4426912853461fe11d2d7062ed071
Len Lee <lenlee454@gmail.com>
2016-09-22 20:53:02 -0700

    hella

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,27 @@
 #write your code here
+def echo(x)
+    x
+end
+
+def shout(x)
+    x.upcase
+end
+
+def repeat(x,y=2)
+    ([x] * y).join(' ')
+end
+
+def start_of_word(word, num)
+    word[0, num]
+end
+
+def first_word(words)
+    words.split[0]
+    
+end
+
+def titleize(str)
+    words = str.split
+    words[0].capitalize!
+    words.map {|word| word=="and" || word=="over" || word=="the" ? word : word.capitalize}.join(" ")
+end
