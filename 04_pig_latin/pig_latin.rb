22aa86ba120c1725d929b245041dd1e9d2bf9e32
Len Lee <lenlee454@gmail.com>
2016-09-27 22:09:12 -0700

    solution

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,31 @@
 #write your code here
+def translate(str)
+    vowels = ['a','e','i','o','u']
+    
+    words = str.split(" ")
+    
+    result = ""
+    
+    words.each do |word|
+        counter = 0
+        if(vowels.include? word[0])
+            counter += 0
+        elsif word[0,2] == "qu"
+            counter += 2
+        else
+            if(vowels.include? word[1])
+                counter += 1
+            elsif(vowels.include? word[2])
+                if(word[1,2] == "qu")
+                    counter += 1
+                end
+                counter += 2
+            else
+                counter += 3
+            end
+        end
+        
+        result = [result, word[counter, word.length] + word[0, counter] + "ay"].join(' ').strip
+    end
+    result
+end
