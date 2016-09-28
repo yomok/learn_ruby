06c8cc3edef881271bfa31e6e0fdbf75acc40be2
Len Lee <lenlee454@gmail.com>
2016-09-28 14:20:37 -0700

    another

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,23 +1,17 @@
 def translate(str)
-    vowels = ['a','e','i','o','u']
-    words = str.split(" ")
-    translated = []
-    words.each do |word|
-        if(vowels.include? word[0])
-            translated << word + "ay"
-        elsif word[0,2] == "qu"
-            translated << word[2..word.length-1] + word[0,2] + "ay"
-        elsif (word[1] == "q")
-            translated << word[3..word.length-1] + word[0,3] + "ay"
-        else
-            pre = ""
-            while(!vowels.include? word[0])
-                pre = pre + word[0]
-                word = word[1..word.length-1]
+    array = str.split(" ")
+    array.each do |word|
+        temp = ""
+        until ["a", "e", "i", "o", "u"].include?(word[0])
+            if (0 < word.length - 1 && word[0] == "q" && word[1] == 'u')
+                temp << word[0,2]
+                word[0,2]=''
+            else
+                temp << word[0]
+                word[0] = ''
             end
-            word = word + pre + "ay"
-            translated << word
         end
-    end 
-    return translated.join(" ")
+        word << temp + "ay"
+    end
+    array.join(" ")
 end
