4e6e38f281d2f73587bb108ed6ab5a2f189b3173
Len Lee <lenlee454@gmail.com>
2016-09-28 19:34:43 -0700

    copied

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+  
+    attr_accessor :title
+    
+    def title=(title)
+        little_words = ['a', 'an', 'and', 'the', 'of', 'in']
+        arr = title.split(' ')
+        first = [arr[0].capitalize!]
+        arr[1..arr.length].each do |x|
+            if little_words.include? x
+                first << x
+            else
+                first << x.capitalize
+            end
+        end
+            @title = first.join(" ")
+    end
 end
